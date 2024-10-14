e37a03f63d30de3cf80ded407e51e1f10e5741d2
岩本 <k_iwamoto@stride-web.co.jp>
2024-10-14 18:53:49 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 // Swiper 制作事例
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 1.4,
     spaceBetween: 30,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+		769: {
+			width:1024,
+			height:768,
+			slidesPerView: 3
+		}
+	}
 });
 
 // ヘッダーのスクロール
@@ -14,5 +21,14 @@ $(window).scroll(function() {
         $("#header").addClass("shadow");
     } else {
         $("#header").removeClass("shadow");
+    }
+});
+
+// ハンバーガーメニュー
+$(".hamburger-icon").click(function() {
+    if($("#header nav").hasClass("show")) {
+        $("#header nav").removeClass("show");
+    } else {
+        $("#header nav").addClass("show");
     }
 });
